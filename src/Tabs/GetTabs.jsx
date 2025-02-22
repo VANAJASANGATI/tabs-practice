778a60876bd1b04e167e1d471836e04dc115215d
S Vanaja <93426943+VANAJASANGATI@users.noreply.github.com>
2025-02-22 06:48:34 +0000

    tab ui changes

--- src/Tabs/GetTabs.jsx
+++ src/Tabs/GetTabs.jsx
@@ -5,6 +5,11 @@ import Settings from "./Settings";
 import "./tabs.css";
 export default function TabForm() {
   const [activeTab, setActiveTab] = useState(0);
+  const [data, setData] = useState({
+    name: "",
+    age: "",
+    email: "",
+  });
   const tabs = [
     {
       tabName: "Profile",
@@ -24,11 +29,33 @@ export default function TabForm() {
   return (
     <>
       <div className="tabs-container">
-        {tabs.map((eachTab) => (
-          <button className="tab">{eachTab.tabName}</button>
+        {tabs.map((eachTab, index) => (
+          <button
+            className="tab"
+            key={index}
+            onClick={() => setActiveTab(index)}
+          >
+            {eachTab.tabName}
+          </button>
         ))}
       </div>
-      <ActiveTabComponent />
+      <div className="component">
+        <ActiveTabComponent data={data} setData={setData} />
+        <div className="button-container">
+          <button
+            disabled={activeTab === 0}
+            onClick={() => setActiveTab((prev) => prev - 1)}
+          >
+            Prev
+          </button>
+          <button
+            disabled={activeTab === tabs.length - 1}
+            onClick={() => setActiveTab((prev) => prev + 1)}
+          >
+            Next
+          </button>
+        </div>
+      </div>
     </>
   );
 }
--- src/Tabs/Profile.jsx
+++ src/Tabs/Profile.jsx
@@ -1,7 +1,55 @@
-export default function Profile() {
+import { useState } from "react";
+export default function Profile({ data, setData }) {
+  console.log("data", data);
+  function handleData(e) {
+    console.log("e", e);
+    setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  }
   return (
-    <div>
-      <h1>Profile</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 10,
+        justifyContent: "flex-start",
+      }}
+    >
+      <div>
+        <label htmlFor="username" style={{ marginRight: 10 }}>
+          Name:
+        </label>
+        <input
+          id="username"
+          name="name"
+          type="text"
+          value={data.name}
+          onChange={handleData}
+        />
+      </div>
+      <div>
+        <label htmlFor="age" style={{ marginRight: 10 }}>
+          Age:
+        </label>
+        <input
+          id="age"
+          name="age"
+          type="number"
+          value={data.age}
+          onChange={handleData}
+        />
+      </div>
+      <div>
+        <label htmlFor="email" style={{ marginRight: 10 }}>
+          Email:
+        </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={data.email}
+          onChange={handleData}
+        />
+      </div>
     </div>
   );
 }
